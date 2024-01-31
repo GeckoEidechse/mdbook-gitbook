b965c50522ddc1d37476f3af3411897e3cf8cfb7
GeckoEidechse <gecko.eidechse+git@pm.me>
2024-01-31 21:00:09 +0100

    refactor: Remove testing bailing code
    The example had some testing code that would purposefully return an error if an arg was passed.

--- src/lib.rs
+++ src/lib.rs
@@ -16,15 +16,7 @@ impl Preprocessor for Nop {
         "nop-preprocessor"
     }
 
-    fn run(&self, ctx: &PreprocessorContext, book: Book) -> Result<Book, Error> {
-        // In testing we want to tell the preprocessor to blow up by setting a
-        // particular config value
-        if let Some(nop_cfg) = ctx.config.get_preprocessor(self.name()) {
-            if nop_cfg.contains_key("blow-up") {
-                anyhow::bail!("Boom!!1!");
-            }
-        }
-
+    fn run(&self, _ctx: &PreprocessorContext, book: Book) -> Result<Book, Error> {
         // we *are* a no-op preprocessor after all
         Ok(book)
     }
